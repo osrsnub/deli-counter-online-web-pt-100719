86fcebd5c123d69e822822e0de77e55fe1a608b3
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-10-14 18:37:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-katz_deli = []
 def line (katz_deli)
   if line = (katz_deli)
     puts "the line is empty"
   end
-  def take a number(katz_deli,number)
+end
+
+def take a number(katz_deli,number)
     puts (katz_deli, number)
-  end
 end
